4984c3200e85885555b52d732acc0f8a6e55e907
717824p103@kce.ac.in <yourmail@gmail.com>
2026-05-24 22:48:00 +0530

    initial commit

--- src/com/wipro/dms/entity/DeliveryAgent.java
+++ src/com/wipro/dms/entity/DeliveryAgent.java
@@ -1,5 +1,20 @@
 package com.wipro.dms.entity;
 
 public class DeliveryAgent {
+	
+	private String agentId;
+	private String name;
+	private boolean active;
+
+public DeliveryAgent(){
+	
+}
+
+
+public DeliveryAgent(String agentId,String name,boolean active) {
+	this.agentId=agentId;
+	this.name=name;
+	this.active=active;
+}
 
 }
--- src/com/wipro/dms/entity/DeliveryUpdate.java
+++ src/com/wipro/dms/entity/DeliveryUpdate.java
@@ -1,5 +1,61 @@
 package com.wipro.dms.entity;
 
 public class DeliveryUpdate {
+	private String updateId;
+    private String deliveryId;
+    private String date;
+    private String notes;
 
+ 
+    public DeliveryUpdate() {
+
+    }
+
+  
+    public DeliveryUpdate(String updateId, String deliveryId, String date, String notes) {
+
+        this.updateId = updateId;
+        this.deliveryId = deliveryId;
+        this.date = date;
+        this.notes = notes;
+    }
+
+   
+    public String getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(String updateId) {
+        this.updateId = updateId;
+    }
+
+  
+    public String getDeliveryId() {
+        return deliveryId;
+    }
+
+  
+    public void setDeliveryId(String deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+
+   
+    public String getDate() {
+        return date;
+    }
+
+   
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+  
+    public String getNotes() {
+        return notes;
+    }
+
+    
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
